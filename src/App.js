78e2a01bd63644e32dd7e578f0153c27417f2db2
pung8146 <pung8146@naver.com>
2023-05-11 21:38:14 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #232123;
+`
+const Title = styled.h1`
+font-size: 60px;
+text-align: center;
+  color:gold;
+`
+
+const ProfilBox = styled.div`
+  width: 500px;
+  height: 500px;
+  background-color: tomato;
+  border-radius: 100px;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <Wrapper>
+      <Title>FrontEnd Develop</Title>
+      <ProfilBox />
+      <Title>PSH</Title>
+     </Wrapper>
   );
 }
 
