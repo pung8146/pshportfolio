472334d39bfc5fdfbfe8e3c7e5a521035807ff0c
pung8146 <pung8146@naver.com>
2023-05-16 13:33:46 +0900

    main page 각각의 div가 움직이게만듬 , 속도 조정이 필요함

--- src/pages/Main copy.js
+++ src/pages/Main copy.js
@@ -1,69 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
+// images
 import Moutin from '../assets/images/Background/Moutin.png'
 import CharacterGif from '../assets/images/Me/MeGif.gif'
-
-// Keyframes for the animation
-const scrollBackground = keyframes`
-  0% {
-    transform: translateX(-100%);
-  }
-  100% {
-    transform: translateX(500%);
-  }
-`;
-
 
 const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
-  background-image: url(${Moutin});
+  background-color: gold;
+  /* background-image: url(${Moutin}); */
   background-repeat: repeat-x;
   background-size: cover;
   display: flex;
   justify-content: center;
   align-items: center;
   overflow: hidden;
-  position: relative;
-`;
-
-const Background1 = styled.div`
-  width: 30%;
-  height: 80vh;
-  position: absolute;
-  bottom:10%;
-  background-color: gold;
-  animation: ${scrollBackground} 10s linear infinite; // Adjust duration as needed
-`;
-
-const Background2 = styled.div`
-  width: 20%;
-  height: 60vh;
-  position: absolute;
-  bottom:20%;
-  left:30%;
-  background-color: yellow;
-  animation: ${scrollBackground} 3s linear infinite; // Adjust duration as needed
-`;
-
-const Background3 = styled.div`
-  width: 10%;
-  height: 40vh;
-  position: absolute;
-  bottom:30%;
-  left:10%;
-  background-color: orange;
-  animation: ${scrollBackground} 3s linear infinite; // Adjust duration as needed
 `;
 
 const Background = styled.div`
-  width: 200%;
+  width: 100%;
   height: 100%;
   background-image: url(${Moutin});
   background-repeat: repeat-x;
   background-size: cover;
-
   animation: scrollBackground 10s linear infinite;
   
   @keyframes scrollBackground {
@@ -140,11 +100,7 @@ const Button = ({ onClick }) => {
 
   return (
     <Wrapper>
-      <Background></Background>
-      <Background1 />
-      <Background1 />
-      <Background2 />
-      <Background3 />
+      <Background />
       <StyledButton
         onClick={onClick}
         whileHover="hover"
