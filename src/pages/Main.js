976c5fea41188c220a26137fd565dc18335dd253
pung8146 <pung8146@naver.com>
2023-05-17 18:03:41 +0900

    클릭시 이동하기 만들기

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import styled, { keyframes } from 'styled-components';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 
 // img
