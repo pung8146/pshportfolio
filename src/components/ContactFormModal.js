6c13cd2eaa19bdd48b4117912e3682925022192b
pung8146 <pung8146@naver.com>
2023-05-23 13:25:58 +0900

    modal 창 1차 디자인

--- src/components/ContactFormModal.js
+++ src/components/ContactFormModal.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Modal from 'react-modal';
+import styled from 'styled-components';
 
 const customStyles = {
   content : {
@@ -8,11 +9,54 @@ const customStyles = {
     right: 'auto',
     bottom: 'auto',
     marginRight: '-50%',
-    transform: 'translate(-50%, -50%)'
+    transform: 'translate(-50%, -50%)',
+    fontFamily: '"Press Start 2P", cursive',
+    color: 'white',
+    background: '#1d1d1b',
+    borderRadius: '5px',
+    border: '2px solid #5c5a5a',
+    padding: '20px',
+    textAlign: 'center'
   }
 };
 
 Modal.setAppElement('#root') // replace with your app element id
+
+const Button = styled.button`
+  font-family: 'Press Start 2P', cursive;
+  color: white;
+  background: #ff006e;
+  border: none;
+  padding: 10px;
+  margin: 10px 0;
+  cursor: pointer;
+  transition: background 0.3s;
+
+  &:hover {
+    background: #ee0979;
+  }
+`;
+
+const Input = styled.input`
+  font-family: 'Press Start 2P', cursive;
+  color: white;
+  background: #1d1d1b;
+  border: 1px solid #5c5a5a;
+  padding: 5px;
+  width: 100%;
+  margin: 10px 0;
+`;
+
+const TextArea = styled.textarea`
+  font-family: 'Press Start 2P', cursive;
+  color: white;
+  background: #1d1d1b;
+  border: 1px solid #5c5a5a;
+  padding: 5px;
+  width: 100%;
+  margin: 10px 0;
+  resize: none;
+`;
 
 function ContactFormModal({ modalIsOpen, setModalIsOpen }) {
   const [form, setForm] = useState({
@@ -37,7 +81,7 @@ function ContactFormModal({ modalIsOpen, setModalIsOpen }) {
 
   return (
     <div>
-      <button onClick={() => setModalIsOpen(true)}>Open Contact Form</button>
+      <Button onClick={() => setModalIsOpen(true)}>Open Contact Form</Button>
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
@@ -48,23 +92,23 @@ function ContactFormModal({ modalIsOpen, setModalIsOpen }) {
         <form onSubmit={handleSubmit}>
           <label>
             Name:
-            <input type="text" name="name" value={form.name} onChange={handleInputChange} required />
+            <Input type="text" name="name" value={form.name} onChange={handleInputChange} required />
           </label>
           <label>
             Email:
-            <input type="email" name="email" value={form.email} onChange={handleInputChange} required />
+            <Input type="email" name="email" value={form.email} onChange={handleInputChange} required />
           </label>
           <label>
             Phone:
-            <input type="tel" name="phone" value={form.phone} onChange={handleInputChange} required />
+            <Input type="tel" name="phone" value={form.phone} onChange={handleInputChange} required />
           </label>
           <label>
             Message:
-            <textarea name="message" value={form.message} onChange={handleInputChange} required />
+            <TextArea name="message" rows="4" value={form.message} onChange={handleInputChange} required />
           </label>
-          <input type="submit" value="Submit" />
+          <Button type="submit">Submit</Button>
         </form>
-        <button onClick={() => setModalIsOpen(false)}>Close</button>
+        <Button onClick={() => setModalIsOpen(false)}>Close</Button>
       </Modal>
     </div>
   );
